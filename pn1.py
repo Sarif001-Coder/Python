d9b21e172bd37c42d39b93fb26a92df2673bc16d
Sarif001-Coder <67628268+Sarif001-Coder@users.noreply.github.com>
2020-06-30 20:29:43 +0530

    Update pn1.py

--- pn1.py
+++ pn1.py
@@ -1,6 +1,6 @@
-In this first file lets talk about variable n data types in python.
+#In this first file lets talk about variable n data types in python.
 
-@)to print anything:
+#to print anything:
 print("hello") 
 op=hello
 
@@ -8,22 +8,26 @@ print(23)
 op=23
 
 
-@)to know the type of any statement.
- let say 
+#to know the type of any statement.
+ #let say 
  s='strings'
  
  print(type(s))
  op=str or string
-  single or multiple char. both will return str in type.
+ 
+ #single or multiple char. both will return str in type.
 
 
-@)Basic types are
+#Basic types are
 -str = strings
 -int = integers
 -float = decimals
 -bool = true/false
 
-@)practice
+#with this speacialcharacter starting given to any line is considered to be Comment
+#there is no concept of multiline comment here.
+
+#practice
 print('12a') => 12a str
 print('10') => 10 str
 print(11.90) => float
