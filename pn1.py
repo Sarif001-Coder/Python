4570060dcc19e65b05d08883ebebf67a4d56f367
Sarif001-Coder <67628268+Sarif001-Coder@users.noreply.github.com>
2020-06-30 20:26:40 +0530

    Create pn1.py

---
+++ pn1.py
@@ -0,0 +1,30 @@
+In this first file lets talk about variable n data types in python.
+
+@)to print anything:
+print("hello") 
+op=hello
+
+print(23)
+op=23
+
+
+@)to know the type of any statement.
+ let say 
+ s='strings'
+ 
+ print(type(s))
+ op=str or string
+  single or multiple char. both will return str in type.
+
+
+@)Basic types are
+-str = strings
+-int = integers
+-float = decimals
+-bool = true/false
+
+@)practice
+print('12a') => 12a str
+print('10') => 10 str
+print(11.90) => float
+print(False) => bool
