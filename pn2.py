25c8177712ff9a824226d8d32ec912c53b5efb0b
SarifSheikh <67628268+Sarif001-Coder@users.noreply.github.com>
2020-07-01 12:56:42 +0530

    Create pn2.py

---
+++ pn2.py
@@ -0,0 +1,28 @@
+#there are 5 relational operators in python.
+#( < ) less than , ( > ) more than
+#( <= )less than equal to , ( >= )more than equal to
+#we can perform comparision of int,float an
+#answer will be in boolean [true/false]
+
+#we can perform comparision of string and we will get answer in boolean.
+
+x=10
+y=20
+print(x<=y)
+#true
+print(x>y)
+#false
+
+s1='hello'
+s2='python'
+print(s1<s2) #comparing string
+
+#function ord , chr -> length of string provided to this must be
+#single character
+s=11
+c1='H'#uppercase character and lowercse character,
+c2='p'# have different unicode values.
+print(ord(c1)) #only single char is allowed
+print(ord(c2))
+
+print(chr(s))
