a174f634cb5429901c0b0350de63a06eedf4c0ef
SarifSheikh <67628268+Sarif001-Coder@users.noreply.github.com>
2020-07-04 00:02:45 +0530

    Create pn3.py

---
+++ pn3.py
@@ -0,0 +1,41 @@
+# there are total 7 arithmetic_operators.
+
+# (+) Addition, (-) subtraction, (*) multiplication
+print(8 + 9)  # 17
+print(9 - 4)  # 5
+print(3 * 4)  # 12
+print("\n")
+print(2.0 + 5.5)  # 7.5
+print(2.0 * 5)  # 10.0
+print("\n")
+
+# (/) division = results are always float.
+print(10 / 2)
+print(20 / 3.0)
+print("\n")
+
+# (%) Modulus
+print(14 % 4)
+print(15.55 % 3)
+print(10 % 3.5)
+print("\n")
+
+# (**) Exponential
+print(2 ** 3)
+print(2.75 ** 3)
+print(2 ** 2.5)
+print("\n")
+
+# (//) Integer/floor division
+print(11 // 2)
+print(15.5 // 4)
+print(9 // 2.2)
+
+# this 3 will throw error as if 2nd operand cant be Zero
+# print(5//0)
+# print(5%0)
+# print(5/0)
+
+# floor & ceil operation[Example]
+# ceil 10.24 => 11 , 11.76 => 12
+# floor 3.45 => 3 , 5.78 => 5
